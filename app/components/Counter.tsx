1e58c66f8aeae06a6374a03455d4e4bb4a764dd9
Auzelg <59143910+Auzelg@users.noreply.github.com>
2023-04-15 20:51:14 +0200

    change counter

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import styles from "./page.module.css";
 
 export default function Counter(props: { handleCounterClick: Function }) {
-  const [initialTime, setInitialTime] = React.useState(15);
+  const [initialTime, setInitialTime] = React.useState(120);
   const [resetTimer, setResetTimer] = React.useState(false);
 
   const { handleCounterClick } = props;
